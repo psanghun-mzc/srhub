a90a87af40583b4ceb6539a195b54f9eabe1af97
박상훈 <psanghun@gmail.com>
2022-12-15 11:19:12 +0900

    Initial commit

---
+++ src/main/java/com/megazonecloud/srhub/SrhubApplication.java
@@ -0,0 +1,13 @@
+package com.megazonecloud.srhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/megazonecloud/srhub/SrhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megazonecloud.srhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
